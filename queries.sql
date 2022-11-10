de2f58d1afbd5a02956a1b350a88c1fd3ccab25b
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-11-10 14:03:32 -0600

    part 2 done

--- queries.sql
+++ queries.sql
@@ -6,6 +6,9 @@ FROM INFORMATION_SCHEMA.COLUMNS
 where TABLE_NAME = 'job';
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
