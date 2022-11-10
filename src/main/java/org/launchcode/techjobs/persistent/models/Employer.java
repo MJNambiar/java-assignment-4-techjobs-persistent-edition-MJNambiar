398e084ee9a3c0ecc37c2ce565583f42e255ade0
MJNambiar <marinanambiar@Nikhils-MacBook-Air.local>
2022-11-10 12:35:39 -0600

    part 2 halfway

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(max = 200, message = "Location cannot be more than 200 characters long")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
